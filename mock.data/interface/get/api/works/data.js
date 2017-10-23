9210db839611b30fa2eab02eb12d78df40cfa1b4
suzichenapk@gmail.com <suzichenapk@gmail.com>
2017-10-23 17:12:37 +0800

    调整了分页器的样式 新增mock数据及过滤器

--- mock.data/interface/get/api/works/data.js
+++ mock.data/interface/get/api/works/data.js
@@ -2,11 +2,12 @@ module.exports = function (json, req) {
 	let result = json.result
 	let query = req.query
 
-	result.total = result.data.length	// 写死了
-	// 由于query是空的，所以一直是10 ↓
-	// result.data = result.data.splice(query.offset, query.limit || 10)
-	result.data = result.data.splice(10, query.limit || 10)
+	if (query.total == '1') {
+		result.total = result.data.length
+	} else {
+		delete result.total
+	}
 
-	result.query = query
+	result.data = result.data.splice(query.offset, query.limit || 10)
 	return json
 }
